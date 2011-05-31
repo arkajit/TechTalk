64b3fd308621b00d7d2557aa7da34aa7f1c389f6
Arkajit Dey <arkajit.dey@gmail.com>
2011-05-30 20:18:02 -0400

    initial android project

---
+++ src/edu/tech/talk/TechTalk.java
@@ -0,0 +1,13 @@
+package edu.tech.talk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TechTalk extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
