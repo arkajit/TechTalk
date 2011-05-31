8dd839f971c01a3d62a876e02433e8a2df6f84e4
Arkajit Dey <arkajit.dey@gmail.com>
2011-05-30 21:02:45 -0400

    basic camera capture functionality

--- gen/edu/tech/talk/R.java
+++ gen/edu/tech/talk/R.java
@@ -13,11 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int sample_pic=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int story_row=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int menu_add=0x7f040003;
+        public static final int no_stories=0x7f040002;
     }
 }
--- src/edu/tech/talk/TechTalk.java
+++ src/edu/tech/talk/TechTalk.java
@@ -1,13 +1,59 @@
 package edu.tech.talk;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class TechTalk extends Activity {
+	
+	private static final int ACTIVITY_CREATE=0;
+	private static final int INSERT_ID = Menu.FIRST;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_add);
+        return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+            case INSERT_ID:
+                createStory();
+                return true;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+    
+    private void createStory() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, ACTIVITY_CREATE);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        
+        switch(requestCode) {
+        case ACTIVITY_CREATE:
+        	Bitmap thumb = (Bitmap) intent.getExtras().get("data");
+        	ImageView img = (ImageView) findViewById(R.id.sample_pic);
+        	img.setImageBitmap(thumb);
+        	break;
+        }
     }
 }
